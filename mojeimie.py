d9fa990d88ed852b0676b294cf1b07c3602bcbf6
student-wroclaw <g.sob78@gmail.com>
2023-03-04 10:02:19 +0100

    anqetil

--- mojeimie.py
+++ mojeimie.py
@@ -1,4 +1,16 @@
 nazwa = 'Kamil Adam Musial'
-nazwa_ze_zmiana = nazwa.replace('i', 'DUZE_I', 1) #co i na ile razy
-#nazwa_bez_spacji = nazwa.replace('')
+nazwa_ze_zmiana = nazwa.replace('i', 'DUZE_I', 1)    #co i na ile razy
 print(nazwa_ze_zmiana)
+
+print(nazwa.replace(' ',''))  #zamien spacje na nic
+print(nazwa.replace(' ','\n'))  #zamien spacje na enter
+
+#nazwa_podzielona = nazwa.split()  #zamiana na listy po spacji
+
+for slowo in nazwa.split():
+    print(slowo)
+
+
+
+
+
