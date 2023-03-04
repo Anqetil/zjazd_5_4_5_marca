5a06c9f36b9f70a2357dc0f845400b4b70971158
student-wroclaw <g.sob78@gmail.com>
2023-03-04 09:52:26 +0100

    anqetil

--- marzec.py
+++ marzec.py
@@ -1,6 +1,9 @@
 #program, który sprawdzi, czy dane słowo to palindron
 #progrm, który działa na liście słow
 
-slowo = input('Wpisz slowo')
-
+slowo = input('Wpisz slowo ')
 liczba_iteracji = len(slowo)//2
+for i in range (liczba_iteracji):
+
+    print(slowo[len(slowo)-1])
+
---
+++ mojeimie.py
@@ -0,0 +1,4 @@
+nazwa = 'Kamil Adam Musial'
+nazwa_ze_zmiana = nazwa.replace('i', 'DUZE_I', 1) #co i na ile razy
+#nazwa_bez_spacji = nazwa.replace('')
+print(nazwa_ze_zmiana)
