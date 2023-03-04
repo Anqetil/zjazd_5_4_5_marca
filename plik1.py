c29105c8c7d64af4767edd48a441fee795eada84
student-wroclaw <g.sob78@gmail.com>
2023-03-04 10:02:19 +0100

    anqetil

---
+++ plik1.py
@@ -0,0 +1,7 @@
+def napisz_czesc():
+    print('no siema')
+
+if __name__ == '__main__':  #wykonaj tylko kiedy plik bezposrednio uruchomiony
+    print('kod z pliku plik1 ',__name__)
+
+print('A to wykonaj zawsze, nawet, kiedy importujez modul')
---
+++ plik2.py
@@ -0,0 +1,4 @@
+from plik1 import *
+print('kod z pliku plik2 ', __name__)
+
+napisz_czesc()
